316fcd75f16e68cd75d9b192785f586e4aa4ff59
patrit <7950125+patrit@users.noreply.github.com>
2024-10-28 14:16:32 +0100

    improve prompt

--- trial.py
+++ trial.py
@@ -4,15 +4,34 @@ import base64
 from openai import OpenAI
 import kagglehub
 from pathlib import Path
+from pydantic import BaseModel
 
 client = OpenAI()
+
+
+class AccidentReport(BaseModel):
+    damage_recognized: bool
+    detailed_damage_description: str
+    car_plate_numbers: list[str]
+
+
+prompt = """You are a helpful car crash assistant called crashy.
+Assemble a detailed report with the given output format.
+Use all provided images and ask for additional images if required.
+Only describe damage specific details of the pictures.
+"""
 
 
 # assert kaggle image data is cached
 def get_dataset() -> list[Path]:
     path = kagglehub.dataset_download("humansintheloop/car-parts-and-car-damages")
     base = Path(path) / "Car damages dataset" / "File1" / "img"
-    return [base / "Car damages 158.png"]
+    return [
+        base / "Car damages 158.png",
+        base / "Car damages 1492.png",
+        base / "Car damages 1335.png",
+        base / "Car damages 250.png",
+    ]
 
 
 # Function to encode the image
@@ -25,23 +44,31 @@ for image_path in get_dataset():
     # Getting the base64 string
     base64_image = encode_image(image_path)
 
-    response = client.chat.completions.create(
+    response = client.beta.chat.completions.parse(
         model="gpt-4o-mini",
         messages=[
             {
-                "role": "user",
+                "role": "system",
                 "content": [
                     {
                         "type": "text",
-                        "text": "What is in this image?",
+                        "text": prompt,
                     },
+                ],
+            },
+            {
+                "role": "user",
+                "content": [
                     {
                         "type": "image_url",
                         "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
                     },
                 ],
-            }
+            },
         ],
+        response_format=AccidentReport,
+        temperature=0.2,
     )
 
     print(response.choices[0])
+    print("")
