4a85bbaa1d0bfced2fa96c86da20f45f95364b51
patrit <7950125+patrit@users.noreply.github.com>
2024-10-28 11:41:58 +0100

    service trial

---
+++ trial.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import base64
+from openai import OpenAI
+
+client = OpenAI()
+
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Path to your image
+image_path = "images/20210607_184450-9a390e08a9eb33dc4518dcafaa64f9d17f9d3096.jpg"
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+response = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "What is in this image?",
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url":  f"data:image/jpeg;base64,{base64_image}"
+          },
+        },
+      ],
+    }
+  ],
+)
+
+print(response.choices[0])
+
