0dcb4f24a784658adda39d25a937d5f618294ed1
patrit <7950125+patrit@users.noreply.github.com>
2024-10-28 13:23:46 +0100

    use kaggle data source

--- trial.py
+++ trial.py
@@ -2,40 +2,48 @@
 
 import base64
 from openai import OpenAI
+import kagglehub
+from pathlib import Path
 
 client = OpenAI()
+
+# assert kaggle image data is cached
+def get_dataset() -> list[Path]:
+  path = kagglehub.dataset_download("humansintheloop/car-parts-and-car-damages")
+  base = Path(path) / "Car damages dataset" / "File1" / "img"
+  return [
+    base  / "Car damages 158.png"
+  ]
 
 # Function to encode the image
 def encode_image(image_path):
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
 
-# Path to your image
-image_path = "images/20210607_184450-9a390e08a9eb33dc4518dcafaa64f9d17f9d3096.jpg"
+for image_path in get_dataset():
+  # Getting the base64 string
+  base64_image = encode_image(image_path)
 
-# Getting the base64 string
-base64_image = encode_image(image_path)
-
-response = client.chat.completions.create(
-  model="gpt-4o-mini",
-  messages=[
-    {
-      "role": "user",
-      "content": [
-        {
-          "type": "text",
-          "text": "What is in this image?",
-        },
-        {
-          "type": "image_url",
-          "image_url": {
-            "url":  f"data:image/jpeg;base64,{base64_image}"
+  response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "text",
+            "text": "What is in this image?",
           },
-        },
-      ],
-    }
-  ],
-)
+          {
+            "type": "image_url",
+            "image_url": {
+              "url":  f"data:image/jpeg;base64,{base64_image}"
+            },
+          },
+        ],
+      }
+    ],
+  )
 
-print(response.choices[0])
+  print(response.choices[0])
 
