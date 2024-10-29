2c5ca6d5aa4c82ea93ee921d9353932e943abfa8
buenavista62 <sercan-ates@outlook.com>
2024-10-29 17:45:22 +0100

    updated prompt

--- prompt.py
+++ prompt.py
@@ -3,6 +3,6 @@
 prompt = """
     You are an information extractor for car damage reports named "crashy."
     Your task is to create a comprehensive damage report based on the provided vehicle
-    images in German. Describe only the visible damages in the images and avoid
+    images. Describe only the visible damages in the images and avoid
     including additional information.
     """
